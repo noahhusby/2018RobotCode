63d6b58b17aa52ca10696dafb49e717c50d1b53b
TimAndyOthers <smwall2002@gmail.com>
2018-01-10 16:29:19 -0500

    began working on the climb code - highly under construction

--- src/main/java/org/usfirst/frc/team1701/robot/commands/StartClimb.java
+++ src/main/java/org/usfirst/frc/team1701/robot/commands/StartClimb.java
@@ -1,4 +1,30 @@
 package org.usfirst.frc.team1701.robot.commands;
 
-public class StartClimb {
+import org.usfirst.frc.team1701.robot.Robot;
+import org.usfirst.frc.team1701.robot.OI;
+import edu.wpi.first.wpilibj.command.Command;
+
+
+public class StartClimb extends Command {
+    //Begin the climb
+/*
+    public StartClimb() {
+        requires(Robot.startClimb);
+    }
+    protected void initalize()
+    {
+
+    }
+    protected void execute()
+    {
+    }
+    protected boolean isFinished()
+    {
+    }
+    protected void end() {}
+    protected void interrupted() {}
+
+
+
+*/
 }
