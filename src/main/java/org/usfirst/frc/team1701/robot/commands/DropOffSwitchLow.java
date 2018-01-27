f914e15c84736527ae6ab7adfbf24ba58e6c562e
Steven <smwall2002@gmail.com>
2018-01-27 12:39:42 -0500

    I don't know

--- src/main/java/org/usfirst/frc/team1701/robot/commands/DropOffSwitchLow.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team1701.robot.commands;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public class DropOffSwitchLow extends Command {
-    public boolean isFinished(){
-        return false;
-    }
-}
--- src/main/java/org/usfirst/frc/team1701/robot/subsystems/Winch.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team1701.robot.subsystems;
-import edu.wpi.first.wpilibj.command.Subsystem;
-
-public class Winch extends Subsystem {
-  public void winchEnd() {}
-  protected void initDefaultCommand() {}
-}
