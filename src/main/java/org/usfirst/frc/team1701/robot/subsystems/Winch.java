5661b8aa732cfc09ecd69257036e3c58817d9e0b
Nicholas Hubbard <hubbardn95@gmail.com>
2018-01-27 14:39:46 -0500

    Modify styling on subsystems.

--- src/main/java/org/usfirst/frc/team1701/robot/subsystems/Winch.java
+++ src/main/java/org/usfirst/frc/team1701/robot/subsystems/Winch.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team1701.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * Well... this is sad.
+ */
 public class Winch extends Subsystem {
   public void winchEnd() {}
   protected void initDefaultCommand() {}
