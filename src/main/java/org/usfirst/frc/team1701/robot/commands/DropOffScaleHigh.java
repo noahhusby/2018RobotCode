9f94fbda99cafa44c82c8d834453b5d4995aa22c
Steven <smwall2002@gmail.com>
2018-02-03 11:36:07 -0500

    worked on the dropoffscalehigh

--- src/main/java/org/usfirst/frc/team1701/robot/commands/DropOffScaleHigh.java
+++ src/main/java/org/usfirst/frc/team1701/robot/commands/DropOffScaleHigh.java
@@ -14,39 +14,25 @@ import org.usfirst.frc.team1701.robot.Robot;
 import org.usfirst.frc.team1701.robot.RobotMap;
 import org.usfirst.frc.team1701.robot.subsystems.LiftArm;
 
-public class DropOffScaleHigh extends Command{
-    private int x = 0;
-    private int scaleHighHeight;
-    //the angle that the scale needs to be for the box to be successfully dropped off
-
-    protected void initalize() {
+public class DropOffScaleHigh extends Command {
+    public double scaleHighHeight = 0.0; // This is the number that the arm angle is at when it has reached the scale height.
+    public double currentHeight; // This is the current height.
+    protected void initialize() {
+        currentHeight = Robot.liftArm.getArmAngle();
     }
-
     protected void execute() {
-
-        //Between the two ===========================================================================
-
-        while(x == 0) {
-            if (Robot.liftArm.getArmAngle() > scaleHighHeight) {
-                RobotMap.winch1.set(1);
-                RobotMap.winch2.set(1);
-
-            } else if (Robot.liftArm.getArmAngle() < scaleHighHeight) {
-                RobotMap.winch1.set(0);
-                RobotMap.winch2.set(0);
-
-            } else {
-                //when the arm is at the scale height needed
-                x = 1;
-            }
-
-        }
-        //===========================================================================================
-        //Above is to get the arm at the right height to do stuff, does not have anything to do with the wrist angle. Yet.
-
+        do {
+            currentHeight = Robot.liftArm.getArmAngle();
+            RobotMap.winch1.set(1);
+            RobotMap.winch2.set(1);
+        } while (currentHeight < scaleHighHeight);
+        // Use the winch to stop the lift.
     }
 
-    public boolean isFinished(){
+    @Override
+    protected boolean isFinished() {
         return false;
     }
+
+
 }
