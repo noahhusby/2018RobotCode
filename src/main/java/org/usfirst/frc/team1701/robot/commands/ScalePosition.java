94938f2ee4fd66911aa7ce51cc5c8c86cea2ad5d
Steven <smwall2002@gmail.com>
2018-02-17 09:53:59 -0500

    Fixed the arm and wrist rotate command thingy for the potentiometer.

--- src/main/java/org/usfirst/frc/team1701/robot/commands/ScalePosition.java
+++ src/main/java/org/usfirst/frc/team1701/robot/commands/ScalePosition.java
@@ -8,6 +8,7 @@
 
 package org.usfirst.frc.team1701.robot.commands;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.Encoder;
 import org.usfirst.frc.team1701.robot.Robot;
@@ -15,16 +16,16 @@ import org.usfirst.frc.team1701.robot.RobotMap;
 import org.usfirst.frc.team1701.robot.subsystems.LiftArm;
 
 public class ScalePosition extends Command {
-    public double scaleHighHeight = 0.0; // This is the number that the arm angle is at when it has reached the scale height.
-    public double currentHeight; // This is the current height.
-    public double minHeight; //this is the bottom of the wrist fold in zone
-    public double maxHeight; //this is the top of the wrist fold in zone
+    public int scaleHighHeight = 0; // This is the number that the arm angle is at when it has reached the scale height.
+    public int currentHeight; // This is the current height.
+    public int minHeight; //this is the bottom of the wrist fold in zone
+    public int maxHeight; //this is the top of the wrist fold in zone
+    private AnalogInput liftArm = RobotMap.liftArmEncoder;
     protected void initialize() {
-        currentHeight = Robot.liftArm.getArmAngle();
     }
     protected void execute() {
         do {
-            currentHeight = Robot.liftArm.getArmAngle();
+            currentHeight = RobotMap.liftArmEncoder.getValue();
             while(true) {
                 RobotMap.winch1.set(1);
                 RobotMap.winch2.set(1);
