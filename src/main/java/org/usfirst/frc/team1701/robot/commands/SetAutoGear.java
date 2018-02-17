c0c9c6af24d3c30206046794250c2300bf47a9dc
Nicholas Hubbard <hubbardn95@gmail.com>
2018-02-17 15:01:24 -0500

    Revert "Revert "Revert "Modify Robot class for generics; add gear status in DriveTrain."""
    This reverts commit ffb454e75ca4c7ef57a0f059b629d0e1b7b997da.

--- src/main/java/org/usfirst/frc/team1701/robot/commands/SetAutoGear.java
+++ src/main/java/org/usfirst/frc/team1701/robot/commands/SetAutoGear.java
@@ -1,17 +1,26 @@
 package org.usfirst.frc.team1701.robot.commands;
+
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team1701.robot.Robot;
+
 public class SetAutoGear extends Command {
     public SetAutoGear() {
         requires(Robot.driveTrain);
     }
-    protected void initialize() {}
+
+    protected void initialize() {
+
+    }
+
     protected void execute() {
         Robot.driveTrain.setAutoGear(true);
     }
+
     protected boolean isFinished() {
         return true;
     }
+
     protected void end() {}
+
     protected void interrupted() {}
 }
